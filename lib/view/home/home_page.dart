b650240fe481e492382e28a9bae732552c1b162f
devberkay <canbr29@gmail.com>
2023-06-14 23:56:41 +0000

    Jun 15, 2023, 2:56 AM

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -21,35 +21,6 @@ class _HomePageState extends ConsumerState<HomePage> {
     final isDismissedValueNotifier = useState(false);
     return Container(
         padding: EdgeInsets.zero,
-        child: ListView(
-          children: isDismissedValueNotifier.value ||
-                  MediaQuery.of(context).size.width < BreakPoints.medium ||
-                  ref.watch(userProvider) != null
-              ? [
-                  Container(
-                      margin: const EdgeInsets.only(top: 50, left: 70),
-                      height: 250,
-                      width: 250,
-                      child: const LandingTitle()),
-                  Container(
-                    margin: const EdgeInsets.only(top: 60, left: 70),
-                    height: 100,
-                    width: 200,
-                    child: const SearchBox(),
-                  )
-                ]
-              : [
-                  DismissibleReminder(isDismissed: isDismissedValueNotifier),
-                  Container(
-                      margin: const EdgeInsets.only(top: 50, left: 50),
-                      height: 250,
-                      width: 250,
-                      child: const LandingTitle()),
-                  Container(
-                    margin: const EdgeInsets.only(top: 60, left: 50),
-                    child: const SearchBox(),
-                  )
-                ],
-        ));
+        child: GridView.count(crossAxisCount: 2));
   }
 }
