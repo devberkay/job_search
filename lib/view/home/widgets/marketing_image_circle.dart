62f83ac39883294e61d3c64011a1e37b51f9b7a5
devberkay <canbr29@gmail.com>
2023-06-15 09:11:03 +0000

    Jun 15, 2023, 12:11 PM

--- lib/view/home/widgets/marketing_image_circle.dart
+++ lib/view/home/widgets/marketing_image_circle.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+final marketingIndexProvider = StateProvider.autoDispose<int>((ref) {
+  return 0;
+});
+
 class MarketingImageCircle extends HookConsumerWidget {
   const MarketingImageCircle({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return CircleAvatar(
-      foregroundImage: NetworkImage(""),
-    );
+
+    return FadeInImage.assetNetwork(placeholder: , image: image);
   }
 }
