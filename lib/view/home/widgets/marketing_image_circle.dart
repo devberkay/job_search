3ebfc7060e911666e487196377b27b4d7aea2bab
devberkay <canbr29@gmail.com>
2023-06-15 09:25:03 +0000

    Jun 15, 2023, 12:25 PM

--- lib/view/home/widgets/marketing_image_circle.dart
+++ lib/view/home/widgets/marketing_image_circle.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final marketingIndexProvider = StateProvider.autoDispose<int>((ref) {
@@ -7,9 +8,35 @@ final marketingIndexProvider = StateProvider.autoDispose<int>((ref) {
 
 class MarketingImageCircle extends HookConsumerWidget {
   const MarketingImageCircle({super.key});
+
+  Widget _imageBuilder(int index) {
+    if(index==0) {
+      
+    }
+
+    else if (index==1)
+
+      {
+
+      }  
+
+    else {
+
+    }
+ 
+ 
+ }
+
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final currentIndex = ref.watch(marketingIndexProvider);
-    return ;
+    final controller0 = useAnimationController();
+    final controller1 = useAnimationController();
+    final controller2 = useAnimationController();
+    return AnimatedBuilder(
+        child : 
+        animation: Listenable.merge([controller0, controller1, controller2]),
+        builder: (context, child) {});
   }
 }
