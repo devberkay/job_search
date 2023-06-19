6b289d7850cea981ebdc85853d0167001ee2b2ea
devberkay <canbr29@gmail.com>
2023-06-19 12:39:50 +0000

    Jun 19, 2023, 3:39 PM

---
+++ lib/view/jobs/widgets/dashboard_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class DashboardView extends HookConsumerWidget {
+  const DashboardView({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
