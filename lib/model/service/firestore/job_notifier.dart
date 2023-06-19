680c1b55be423a84e3d892a79f5d83de0c8e9661
devberkay <canbr29@gmail.com>
2023-06-19 21:36:47 +0000

    Jun 20, 2023, 12:36 AM

--- lib/model/service/firestore/job_notifier.dart
+++ lib/model/service/firestore/job_notifier.dart
@@ -65,18 +65,52 @@ class JobNotifier extends AutoDisposeAsyncNotifier<List<JobModel>?> {
             responsibilities: responsibilities));
       }
       ref.read(lastJobDocProvider.notifier).state = query.docs.last;
-      debugPrint("jobModels : $jobModels");
+      debugPrint("jobModels-above : $jobModels");
       return jobModels;
     } else {
       debugPrint("jobNotifier-2");
       final query = await collectionRef.limit(15).get();
-      debugPrint("jobNotifier-3");
-      final jobModels = query.docs.map((e) {
-        return JobModel.fromJson(e.data()).copyWith(jobId: e.id);
-      }).toList();
-      debugPrint("jobNotifier-4");
+      List<JobModel> jobModels = [];
+      List<Future<QuerySnapshot<Map<String, dynamic>>>> qualificationsFutures =
+          [];
+      for (var doc in query.docs) {
+        final docId = doc.id;
+        final minimumQualificationsCollectionRef =
+            firestore.collection("jobPosts/$docId/minimumQualifications");
+        final preferredQualificationsCollectionRef =
+            firestore.collection("jobPosts/$docId/preferredQualifications");
+        final responsibilitiesCollectionRef =
+            firestore.collection("jobPosts/$docId/responsibilities");
+        qualificationsFutures.addAll([
+          minimumQualificationsCollectionRef.get(),
+          preferredQualificationsCollectionRef.get(),
+          responsibilitiesCollectionRef.get()
+        ]);
+      }
+      final compoundData = await Future.wait(qualificationsFutures);
+      for (var i = 0; i < query.docs.length; i++) {
+        final doc = query.docs[i];
+        final docId = doc.id;
+        final minimumQualificationsQuery = compoundData[i * 3];
+        final preferredQualificationsQuery = compoundData[i * 3 + 1];
+        final responsibilitiesQuery = compoundData[i * 3 + 2];
+        final minimumQualifications = minimumQualificationsQuery.docs
+            .map((e) => (e.data()["qualifications"] as List<String>).join(" "))
+            .toList();
+        final preferredQualifications = preferredQualificationsQuery.docs
+            .map((e) => (e.data()["qualifications"] as List<String>).join(" "))
+            .toList();
+        final responsibilities = responsibilitiesQuery.docs
+            .map((e) => (e.data()["qualifications"] as List<String>).join(" "))
+            .toList();
+        jobModels.add(JobModel.fromJson(doc.data()).copyWith(
+            jobId: docId,
+            minimumQualifications: minimumQualifications,
+            preferredQualifications: preferredQualifications,
+            responsibilities: responsibilities));
+      }
       ref.read(lastJobDocProvider.notifier).state = query.docs.last;
-      // debugPrint("jobModels : $jobModels");
+      debugPrint("jobModels-below : $jobModels");
       return jobModels;
     }
   }
