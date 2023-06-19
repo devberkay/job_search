3db72573d18bee62e145a1d11ebb1381e266242e
devberkay <canbr29@gmail.com>
2023-06-19 06:18:39 +0000

    Jun 19, 2023, 9:18 AM

--- lib/view/jobs/widgets/filter_sidebar.dart
+++ lib/view/jobs/widgets/filter_sidebar.dart
@@ -1,3 +1,5 @@
+import 'package:JobSearch/view/shared/filled_cupertino_button.dart';
+import 'package:JobSearch/view/shared/headless_cupertino_button.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -8,9 +10,26 @@ class FilterSidebar extends HookConsumerWidget {
     return Container(
       height: double.maxFinite,
       width: 300,
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
       decoration: BoxDecoration(
         color: Colors.white,
-        
+        border: Border(right: BorderSide(color: Colors.grey.shade400))
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              const Text("What do you want to do?",style:TextStyle(fontWeight: FontWeight.w900)),
+              const Spacer(),
+              HeadlessCupertinoButton(child:Text("Clear filters",style: TextStyle(fontWeight: FontWeight.w600,color: Colors.blueAccent.shade700)), onPressed: () {})
+            ],
+          ),
+          const SizedBox(height:15),
+           ExpansionPanelList(
+            dividerColor: Colors.grey.shade400,
+            
+          )
+        ],
       ),
     );
   }
