14ddd713adf0032bd38c421a79c05f9bcd6a007b
devberkay <canbr29@gmail.com>
2023-06-19 06:26:09 +0000

    Jun 19, 2023, 9:26 AM

--- lib/view/jobs/widgets/filter_sidebar.dart
+++ lib/view/jobs/widgets/filter_sidebar.dart
@@ -1,10 +1,18 @@
 import 'package:JobSearch/view/shared/filled_cupertino_button.dart';
 import 'package:JobSearch/view/shared/headless_cupertino_button.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class FilterSidebar extends HookConsumerWidget {
   const FilterSidebar({super.key});
+
+
+  Widget _headerBuilder(BuildContext context, bool isExpanded) {
+    
+  }
+
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Container(
@@ -25,9 +33,30 @@ class FilterSidebar extends HookConsumerWidget {
             ],
           ),
           const SizedBox(height:15),
+          CupertinoTextField(
+            placeholder: "Software Engineering, Design, Sales",
+            placeholderStyle: TextStyle(color: Colors.grey.shade400.withOpacity(0.25),fontWeight: FontWeight.w500),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade400.withOpacity(0.25),
+                  offset: const Offset(0, 2),
+                  blurRadius: 2
+                )
+              ],
+              borderRadius: BorderRadius.circular(5)
+            ),
+          ),
+          const SizedBox(height:15),
            ExpansionPanelList(
+            expansionCallback: (panelIndex, isExpanded) {
+              
+            },
             dividerColor: Colors.grey.shade400,
-            
+            children: [
+              ExpansionPanel(headerBuilder: _headerBuilder, body: body)
+            ],
           )
         ],
       ),
