8b8270363657eac3dc9d1531ece09bac26c479b7
devberkay <canbr29@gmail.com>
2023-06-19 20:20:47 +0000

    Jun 19, 2023, 11:20 PM

--- lib/model/service/firestore/minimum_qualifications_notifier.dart
+++ lib/model/service/firestore/minimum_qualifications_notifier.dart
@@ -1,14 +1,16 @@
 import 'dart:async';
 
+import 'package:JobSearch/model/provider/firestore/firestore_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-
-
-
-
-class MinimumQualificationsNotifier extends AutoDisposeFamilyAsyncNotifier<List<String>?,String> {
+class MinimumQualificationsNotifier
+    extends AutoDisposeFamilyAsyncNotifier<List<String>?, String> {
   @override
-  FutureOr<List<String>?> build(String arg) async { // arg here is the job document id
-    return null;
+  FutureOr<List<String>?> build(String arg) async {
+    // arg here is the job document id
+    final firestore = ref.read(firestoreProvider);
+    final collectionRef = firestore.collection("jobPosts/$arg/minimumQualifications");
+    final query = await collectionRef.get();
+    return query.docs.map((e) => e.data()["qualifications"]).toList();
   }
 }
