a8ebd9c0ada9cef6154b3c766e48c8bad22694d5
devberkay <canbr29@gmail.com>
2023-06-19 15:00:58 +0000

    Jun 19, 2023, 6:00 PM

--- lib/view/jobs/widgets/job_card.dart
+++ lib/view/jobs/widgets/job_card.dart
@@ -3,10 +3,11 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class JobCard extends HookConsumerWidget {
   const JobCard({super.key});
+  
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return InkWell(child: Container(
-      
+
     ));
   }
 }
