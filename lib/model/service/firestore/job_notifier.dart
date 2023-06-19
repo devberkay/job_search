7f8c41b8ae19f0e3c4013e8f024145261a5d757d
devberkay <canbr29@gmail.com>
2023-06-19 16:03:29 +0000

    Jun 19, 2023, 7:03 PM

--- lib/model/service/firestore/job_notifier.dart
+++ lib/model/service/firestore/job_notifier.dart
@@ -2,15 +2,24 @@ import 'dart:async';
 
 import 'package:JobSearch/model/data/job_model.dart';
 import 'package:JobSearch/model/provider/firestore/firestore_provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-
+final lastJobDocProvider = StateProvider<DocumentSnapshot?>((ref) {
+  return null;
+});
 
 class JobNotifier extends AsyncNotifier<List<JobModel>?> {
   @override
   FutureOr<List<JobModel>?> build() {
-    final firestore = ref.read(firestoreProvider);
+    final firestore = ref.watch(firestoreProvider);
     final collectionRef = firestore.collection("jobPosts");
-    
+    final lastJobDoc = ref.watch(lastJobDocProvider);
+    if(lastJobDoc!=null) {
+      collectionRef.startAfterDocument(lastJobDoc);
+    }
+    else {
+      collectionRef.
+    }
   }
 }
