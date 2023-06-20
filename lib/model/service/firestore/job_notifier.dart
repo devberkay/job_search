cb68060277274784cb29ff6273dced6e301712a1
devberkay <canbr29@gmail.com>
2023-06-20 18:04:10 +0000

    Jun 20, 2023, 9:04 PM

--- lib/model/service/firestore/job_notifier.dart
+++ lib/model/service/firestore/job_notifier.dart
@@ -6,74 +6,84 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-
 final orderByProvider = StateProvider<String?>((ref) {
   return null;
 });
 
-final whatDoYouWantToDoListProvider = NotifierProvider<WhatDoYouWantToDoListNotifier,List<String>>(WhatDoYouWantToDoListNotifier.new);
+final whatDoYouWantToDoListProvider =
+    NotifierProvider<WhatDoYouWantToDoListNotifier, List<String>>(
+        WhatDoYouWantToDoListNotifier.new);
 
 class WhatDoYouWantToDoListNotifier extends Notifier<List<String>> {
   @override
-   build() {
+  build() {
     return [];
   }
-  void add(List<String> value) {
-    state = [...state,...value];
-  }
-  void remove(String value) {
-    state = [...state]..remove(value);
-  }
-}
 
-final skillsListProvider = NotifierProvider<SkillsListNotifier, List<String>>(SkillsListNotifier.new);
-
-
-class SkillsListNotifier extends Notifier<List<String>> {
-  @override
-   build() {
-    return [];
-  }
   void add(List<String> value) {
     state = [...state, ...value];
   }
+
   void remove(String value) {
     state = [...state]..remove(value);
   }
 }
 
+final skillsListProvider =
+    NotifierProvider<SkillsListNotifier, List<String>>(SkillsListNotifier.new);
 
-final degreesSetProvider = NotifierProvider<DegreesSetNotifier,Set<String> >(DegreesSetNotifier.new);
+class SkillsListNotifier extends Notifier<List<String>> {
+  @override
+  build() {
+    return [];
+  }
+
+  void add(List<String> value) {
+    state = [...state, ...value];
+  }
+
+  void remove(String value) {
+    state = [...state]..remove(value);
+  }
+}
+
+final degreesSetProvider =
+    NotifierProvider<DegreesSetNotifier, Set<String>>(DegreesSetNotifier.new);
 
 class DegreesSetNotifier extends Notifier<Set<String>> {
   @override
-   build() {
+  build() {
     return {};
   }
+
   void add(String value) {
+    ref.invalidate(jobTypesSetProvider);
     state = {...state, value};
   }
+
   void remove(String value) {
     state = {...state}..remove(value);
   }
 }
 
-
-final jobTypesSetProvider = NotifierProvider<JobTypesSetNotifier,Set<String> >(JobTypesSetNotifier.new);
+final jobTypesSetProvider =
+    NotifierProvider<JobTypesSetNotifier, Set<String>>(JobTypesSetNotifier.new);
 
 class JobTypesSetNotifier extends Notifier<Set<String>> {
   @override
-   build() {
+  build() {
     return {};
   }
+
   void add(String value) {
+    ref.invalidate(degreesSetProvider);
     state = {...state, value};
   }
+
   void remove(String value) {
     state = {...state}..remove(value);
   }
 }
-
 
 final isRemoteEligibleProvider = StateProvider<bool>((ref) {
   return false;
@@ -83,11 +93,11 @@ final lastJobDocProvider = StateProvider<DocumentSnapshot?>((ref) {
   return null;
 });
 
-final jobNotifierProvider =
-    AsyncNotifierProvider.autoDispose.family<JobNotifier, List<JobModel>?,bool>(
-        JobNotifier.new);
+final jobNotifierProvider = AsyncNotifierProvider.autoDispose
+    .family<JobNotifier, List<JobModel>?, bool>(JobNotifier.new);
 
-class JobNotifier extends AutoDisposeFamilyAsyncNotifier<List<JobModel>?,bool> {
+class JobNotifier
+    extends AutoDisposeFamilyAsyncNotifier<List<JobModel>?, bool> {
   @override
   FutureOr<List<JobModel>?> build(bool arg) async {
     final orderBy = ref.watch(orderByProvider);
@@ -141,81 +151,84 @@ class JobNotifier extends AutoDisposeFamilyAsyncNotifier<List<JobModel>?,bool> {
 
     final firestore = ref.watch(firestoreProvider);
     var collectionRef = firestore.collection("jobPosts");
-    Query<Map<String,dynamic>>? query=null;
-    
+    Query<Map<String, dynamic>>? query = null;
+
     final lastJobDoc = ref.read(lastJobDocProvider);
     if (degreesFilterSet.isNotEmpty) {
-      if(query!=null) {
+      if (query != null) {
         query = query.where("degree", whereIn: degreesFilterSet);
-      }
-      else {
+      } else {
         query = collectionRef.where("degree", whereIn: degreesFilterSet);
       }
     }
     if (jobTypesFilterSet.isNotEmpty) {
-     if(query!=null) {
+      if (query != null) {
         query = query.where("jobType", whereIn: jobTypesFilterSet);
-      }
-      else {
+      } else {
         query = collectionRef.where("jobType", whereIn: jobTypesFilterSet);
       }
     }
-    if(isRemoteEligibleFilterBoolean) {
-      if(query!=null) {
-        query = query.where("isRemote", isEqualTo: isRemoteEligibleFilterBoolean);
-      }
-      else {
-        query = collectionRef.where("isRemote", isEqualTo: isRemoteEligibleFilterBoolean);
-      }
-    }
-    if(whatDoYouWantToDoFilterList.isNotEmpty) {
-      if(query!=null) {
-        query = query.where("titleTokens", arrayContainsAny: whatDoYouWantToDoFilterList);
-      }
-      else {
-        query = collectionRef.where("titleTokens", arrayContainsAny: whatDoYouWantToDoFilterList);
+    if (isRemoteEligibleFilterBoolean) {
+      if (query != null) {
+        query =
+            query.where("isRemote", isEqualTo: isRemoteEligibleFilterBoolean);
+      } else {
+        query = collectionRef.where("isRemote",
+            isEqualTo: isRemoteEligibleFilterBoolean);
       }
     }
-    if(skillsFilterList.isNotEmpty) {
-      if(query!=null) {
+    if (whatDoYouWantToDoFilterList.isNotEmpty) {
+      if (query != null) {
+        query = query.where("titleTokens",
+            arrayContainsAny: whatDoYouWantToDoFilterList);
+      } else {
+        query = collectionRef.where("titleTokens",
+            arrayContainsAny: whatDoYouWantToDoFilterList);
+      }
+    }
+    if (skillsFilterList.isNotEmpty) {
+      if (query != null) {
         query = query.where("searchTokens", arrayContainsAny: skillsFilterList);
-      }
-      else {
-        query = collectionRef.where("searchTokens", arrayContainsAny: skillsFilterList);
+      } else {
+        query = collectionRef.where("searchTokens",
+            arrayContainsAny: skillsFilterList);
       }
     }
-    if(orderBy!=null) {
-      if(query!=null) {
-        query = query.orderBy(orderBy,descending:true);
-      }
-      else {
-         query = collectionRef.orderBy(orderBy,descending:true);
+    if (orderBy != null) {
+      if (query != null) {
+        query = query.orderBy(orderBy, descending: true);
+      } else {
+        query = collectionRef.orderBy(orderBy, descending: true);
       }
     }
 
     if (lastJobDoc != null && arg) {
       debugPrint("jobNotifier-0");
-      
-      final snapshot =
-          await (query!=null ? query.startAfterDocument(lastJobDoc).limit(15).get() : collectionRef.startAfterDocument(lastJobDoc).limit(15).get());
+
+      final snapshot = await (query != null
+          ? query.startAfterDocument(lastJobDoc).limit(15).get()
+          : collectionRef.startAfterDocument(lastJobDoc).limit(15).get());
       debugPrint("jobNotifier-1");
       final jobModels = snapshot.docs.map((e) {
         return JobModel.fromJson(e.data()).copyWith(jobId: e.id);
       }).toList();
-      ref.read(lastJobDocProvider.notifier).state = snapshot.docs.isNotEmpty ? snapshot.docs.last : null;
+      ref.read(lastJobDocProvider.notifier).state =
+          snapshot.docs.isNotEmpty ? snapshot.docs.last : null;
       // debugPrint("jobModels : $jobModels");
       return jobModels;
     } else {
       debugPrint("jobNotifier-2");
 
-      final snapshot =
-          await (query!=null ? query.limit(15).get() : collectionRef.limit(15).get());
+      final snapshot = await (query != null
+          ? query.limit(15).get()
+          : collectionRef.limit(15).get());
       debugPrint("jobNotifier-3");
       final jobModels = snapshot.docs.map((e) {
         return JobModel.fromJson(e.data()).copyWith(jobId: e.id);
       }).toList();
       debugPrint("jobNotifier-4");
-      ref.read(lastJobDocProvider.notifier).state = snapshot.docs.isNotEmpty ? snapshot.docs.last : null;
+      ref.read(lastJobDocProvider.notifier).state =
+          snapshot.docs.isNotEmpty ? snapshot.docs.last : null;
       debugPrint("jobModels : $jobModels");
       return jobModels;
     }
