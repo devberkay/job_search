d39b5d7a02b1105df6cf15b9ad0d00cecb774cf3
devberkay <canbr29@gmail.com>
2023-06-20 18:52:42 +0000

    Jun 20, 2023, 9:52 PM

--- lib/view/profile/widgets/profile_sidebar.dart
+++ lib/view/profile/widgets/profile_sidebar.dart
@@ -11,23 +11,48 @@ class ProfileSidebar extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Container(
-      height: double.infinity,
-      width: 400,
-      padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 10),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border(right: BorderSide(color: Colors.grey.shade400))),
-      child:Column(
-        children: [
-          HookConsumer(
-            builder: (context,ref,child) {
-              return FilledCupertinoButton(child: Row(
-                children: [Icon(Icons.add),SizedBox(width: 5),Text("Create job post")],
-              ), onPressed: () {}, height: 100, width: double.maxFinite, fillColor: Colors.white, borderRadius: BorderRadius.circular(10));
-            }
-          )
-        ],
-      )
-    );
+        height: double.infinity,
+        width: 400,
+        color: Colors.grey.shade100.withOpacity(0.25),
+        padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 10),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border(right: BorderSide(color: Colors.grey.shade400))),
+        child: Column(
+          children: [
+            HookConsumer(builder: (context, ref, child) {
+              final isSelected = ref.watch(sidebarIndexProvider.select((value) => value==0));
+              return FilledCupertinoButton(
+                  onPressed: () {},
+                  height: 100,
+                  width: double.maxFinite,
+                  fillColor: isSelected ? Colors.blueAccent.shade700 : Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  child: const Row(
+                    children: [
+                      Icon(Icons.edit),
+                      SizedBox(width: 5),
+                      Text("Edit profile")
+                    ],
+                  ));
+            }),
+            HookConsumer(builder: (context, ref, child) {
+              final isSelected = ref.watch(sidebarIndexProvider.select((value) => value==0));
+              return FilledCupertinoButton(
+                  onPressed: () {},
+                  height: 100,
+                  width: double.maxFinite,
+                  fillColor: isSelected ? Colors.blueAccent.shade700 : Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  child: const Row(
+                    children: [
+                      Icon(Icons.add),
+                      SizedBox(width: 5),
+                      Text("Create job post")
+                    ],
+                  ));
+            })
+          ],
+        ));
   }
 }
