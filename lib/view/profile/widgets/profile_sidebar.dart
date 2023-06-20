85a461dc9d163560536c4c5a92e8e06c37839487
devberkay <canbr29@gmail.com>
2023-06-20 18:50:10 +0000

    Jun 20, 2023, 9:50 PM

--- lib/view/profile/widgets/profile_sidebar.dart
+++ lib/view/profile/widgets/profile_sidebar.dart
@@ -1,5 +1,10 @@
+import 'package:JobSearch/view/shared/filled_cupertino_button.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final sidebarIndexProvider = StateProvider<int>((ref) {
+  return 0;
+});
 
 class ProfileSidebar extends HookConsumerWidget {
   const ProfileSidebar({super.key});
@@ -12,6 +17,17 @@ class ProfileSidebar extends HookConsumerWidget {
       decoration: BoxDecoration(
           color: Colors.white,
           border: Border(right: BorderSide(color: Colors.grey.shade400))),
+      child:Column(
+        children: [
+          HookConsumer(
+            builder: (context,ref,child) {
+              return FilledCupertinoButton(child: Row(
+                children: [Icon(Icons.add),SizedBox(width: 5),Text("Create job post")],
+              ), onPressed: () {}, height: 100, width: double.maxFinite, fillColor: Colors.white, borderRadius: BorderRadius.circular(10));
+            }
+          )
+        ],
+      )
     );
   }
 }
