a5b5030dbee1ccff80764f2576af0d9aaf4e1b56
devberkay <canbr29@gmail.com>
2023-06-20 23:46:42 +0000

    Jun 21, 2023, 2:46 AM

--- lib/model/service/firestore/user_model_service_notifier.dart
+++ lib/model/service/firestore/user_model_service_notifier.dart
@@ -1,10 +1,13 @@
 import 'dart:async';
 
 import 'package:JobSearch/model/data/user_model.dart';
+import 'package:JobSearch/model/provider/auth/user_model_provider.dart';
 import 'package:JobSearch/model/provider/firestore/firestore_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final userModelServiceProvider = AsyncNotifierProvider.autoDispose<UserModelServiceNotifier,void >(UserModelServiceNotifier.new);
+final userModelServiceProvider =
+    AsyncNotifierProvider.autoDispose<UserModelServiceNotifier, void>(
+        UserModelServiceNotifier.new);
 
 class UserModelServiceNotifier extends AutoDisposeAsyncNotifier<void> {
   @override
@@ -16,6 +19,7 @@ class UserModelServiceNotifier extends AutoDisposeAsyncNotifier<void> {
     final collectionRef = firestore.collection("users");
     final docRef = collectionRef.doc(draftUserModel.uid);
     await docRef.update(draftUserModel.toJson());
+    ref.invalidate(userModelProvider);
     state = AsyncData(null);
   }
 }
