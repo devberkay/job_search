cd016bb311fb72d7f5ac07409a5db7e4dcc9ca15
devberkay <canbr29@gmail.com>
2023-06-20 00:23:18 +0000

    Jun 20, 2023, 3:23 AM

--- lib/model/service/firestore/job_notifier.dart
+++ lib/model/service/firestore/job_notifier.dart
@@ -6,6 +6,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+
+final orderByProvider = StateProvider<>((ref) {
+  return ;
+});
+
+
 final lastJobDocProvider = StateProvider<DocumentSnapshot?>((ref) {
   return null;
 });
