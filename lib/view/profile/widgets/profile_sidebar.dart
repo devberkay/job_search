a367a62e1ab28faec1f2ae358725866391c4521a
devberkay <canbr29@gmail.com>
2023-06-20 18:44:40 +0000

    Jun 20, 2023, 9:44 PM

--- lib/view/profile/widgets/profile_sidebar.dart
+++ lib/view/profile/widgets/profile_sidebar.dart
@@ -5,6 +5,13 @@ class ProfileSidebar extends HookConsumerWidget {
   const ProfileSidebar({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return Container(
+      height: double.infinity,
+      width: 400,
+      padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 10),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(right: BorderSide(color: Colors.grey.shade400))),
+    );
   }
 }
