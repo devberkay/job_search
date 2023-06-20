68095fcbd1b0475e7ccabb01f63c9d946f1c247b
devberkay <canbr29@gmail.com>
2023-06-20 21:01:41 +0000

    Jun 21, 2023, 12:01 AM

--- lib/view/profile/widgets/profile_dashboard_notifications_view.dart
+++ lib/view/profile/widgets/profile_dashboard_notifications_view.dart
@@ -1,8 +1,10 @@
+import 'package:JobSearch/model/data/application_model.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ProfileDashboardNotificationsView extends HookConsumerWidget {
-  const ProfileDashboardNotificationsView({super.key});
+  const ProfileDashboardNotificationsView({super.key,required this.applications});
+  final List<ApplicationModel> applications;
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Container();
