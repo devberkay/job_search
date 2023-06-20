739c5f9e720808a9ab59dce6df5ff2d11a1f72c6
devberkay <canbr29@gmail.com>
2023-06-20 20:54:41 +0000

    Jun 20, 2023, 11:54 PM

---
+++ lib/view/profile/widgets/profile_dashboard_manage_job_posts_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class ProfileDashboardManageJobPostsView extends HookConsumerWidget {
+  const ProfileDashboardManageJobPostsView({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
