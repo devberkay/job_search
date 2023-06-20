2bb14ba0badfc8500a3438c110f3fc7dbc8cbb2e
devberkay <canbr29@gmail.com>
2023-06-20 21:21:11 +0000

    Jun 21, 2023, 12:21 AM

--- lib/view/profile/widgets/profile_dashboard_profile_view.dart
+++ lib/view/profile/widgets/profile_dashboard_profile_view.dart
@@ -1,8 +1,15 @@
 import 'package:JobSearch/model/data/user_model.dart';
 import 'package:JobSearch/model/provider/auth/user_provider.dart';
 import 'package:JobSearch/view/shared/profile_avatar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final draftUserModelProvider = StateProvider<UserModel?>((ref) {
+  return null;
+});
+
+
 
 class ProfileDashboardProfileView extends HookConsumerWidget {
   const ProfileDashboardProfileView({super.key, required this.usermodel});
@@ -19,7 +26,44 @@ class ProfileDashboardProfileView extends HookConsumerWidget {
       return Column(
         children: [
           ProfileAvatar(radius: 50, userId: usermodel.uid),
-          
+          SizedBox(height: 30),
+          Row(
+            children: [
+              Column(
+                children: [
+                  Text("Name"),
+                  SizedBox(height: 10),
+                  CupertinoTextField(
+                    readOnly: true,
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    controller: TextEditingController(text: usermodel.name),
+                  )
+                ],
+              ),
+              Spacer(),
+              Column(
+                children: [
+                  Text("Last name"),
+                  SizedBox(height: 10),
+                  CupertinoTextField(
+                    readOnly: true,
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    controller: TextEditingController(text: usermodel.name),
+                  )
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 30),
+
         ],
       );
     }
