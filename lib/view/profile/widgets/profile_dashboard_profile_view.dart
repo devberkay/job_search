a0ed44440ab0e69f07f27ff72657cefb2e926a95
devberkay <canbr29@gmail.com>
2023-06-20 21:32:11 +0000

    Jun 21, 2023, 12:32 AM

--- lib/view/profile/widgets/profile_dashboard_profile_view.dart
+++ lib/view/profile/widgets/profile_dashboard_profile_view.dart
@@ -5,11 +5,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final draftUserModelProvider = StateProvider<UserModel?>((ref) {
+final draftUserModelProvider = StateProvider.autoDispose<UserModel?>((ref) {
   return null;
 });
-
-
 
 class ProfileDashboardProfileView extends HookConsumerWidget {
   const ProfileDashboardProfileView({super.key, required this.usermodel});
@@ -17,27 +15,26 @@ class ProfileDashboardProfileView extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(draftUserModelProvider, (previous, next) {});
     final user = ref.watch(userProvider);
     final isSelf = usermodel.uid == user!.uid;
-    if(isSelf) {
-      return Container();
-    }
-    else {
+    if (isSelf) {
       return Column(
         children: [
           ProfileAvatar(radius: 50, userId: usermodel.uid),
-          SizedBox(height: 30),
+          const SizedBox(height: 30),
           Row(
             children: [
               Column(
                 children: [
-                  Text("Name"),
-                  SizedBox(height: 10),
+                  const Text("Name",style: TextStyle(fontWeight: FontWeight.w900)),
+                  const SizedBox(height: 10),
                   CupertinoTextField(
                     onChanged: (value) {
-                      ref.read(draftUserModelProvider.notifier).state = usermodel.copyWith(name: value);
+                      ref.read(draftUserModelProvider.notifier).state =
+                          usermodel.copyWith(name: value);
                     },
-                    padding: EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
                     decoration: BoxDecoration(
                       color: Colors.grey.shade200,
                       borderRadius: BorderRadius.circular(10),
@@ -46,14 +43,17 @@ class ProfileDashboardProfileView extends HookConsumerWidget {
                   )
                 ],
               ),
-              Spacer(),
+              const Spacer(),
               Column(
                 children: [
-                  Text("Last name"),
-                  SizedBox(height: 10),
+                  const Text("Last name",style: TextStyle(fontWeight: FontWeight.w900)),
+                  const SizedBox(height: 10),
                   CupertinoTextField(
-                    readOnly: true,
-                    padding: EdgeInsets.all(10),
+                    onChanged: (value) {
+                      ref.read(draftUserModelProvider.notifier).state =
+                          usermodel.copyWith(surname: value);
+                    },
+                    padding: const EdgeInsets.all(10),
                     decoration: BoxDecoration(
                       color: Colors.grey.shade200,
                       borderRadius: BorderRadius.circular(10),
@@ -64,10 +64,52 @@ class ProfileDashboardProfileView extends HookConsumerWidget {
               ),
             ],
           ),
-          SizedBox(height: 30),
-
+          const SizedBox(height: 30),
+          Row(
+            children: [
+              Column(
+                children: [
+                  const Text("Phone",style: TextStyle(fontWeight: FontWeight.w900)),
+                  const SizedBox(height: 10),
+                  CupertinoTextField(
+                    onChanged: (value) {
+                      ref.read(draftUserModelProvider.notifier).state =
+                          usermodel.copyWith(phone: value);
+                    },
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    controller: TextEditingController(text: usermodel.name),
+                  )
+                ],
+              ),
+              const Spacer(),
+              Column(
+                children: [
+                  const Text("Mail",style: TextStyle(fontWeight: FontWeight.w900)),
+                  const SizedBox(height: 10),
+                  CupertinoTextField(
+                    onChanged: (value) {
+                      ref.read(draftUserModelProvider.notifier).state =
+                          usermodel.copyWith(mail: value);
+                    },
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    controller: TextEditingController(text: usermodel.name),
+                  )
+                ],
+              ),
+            ],
+          ),
         ],
       );
+    } else {
+      return Container();
     }
   }
 }
