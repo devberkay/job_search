2a1719c14dbf1cef9a9bc0667ae0a3b60acaef91
devberkay <canbr29@gmail.com>
2023-06-20 19:41:40 +0000

    Jun 20, 2023, 10:41 PM

--- lib/view/profile/widgets/profile_dashboard_view.dart
+++ lib/view/profile/widgets/profile_dashboard_view.dart
@@ -3,6 +3,22 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ProfileDashboardView extends HookConsumerWidget {
   const ProfileDashboardView({super.key});
+
+  Widget _buildProfileDashboard(int index) {
+    switch (index) {
+      case 0:
+        return Container();
+      case 1:
+        return Container();
+      case 2:
+        return Container();
+      case 3:
+        return Container();
+      default:
+        return Container();
+    }
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Container();
