07139e31b1dbc6bc7a8d0e2e97f5b8d486a7f96b
devberkay <canbr29@gmail.com>
2023-06-21 17:54:12 +0000

    Jun 21, 2023, 8:54 PM

--- lib/model/provider/storage/raw_picture_provider.dart
+++ lib/model/provider/storage/raw_picture_provider.dart
@@ -3,16 +3,16 @@ import 'dart:typed_data';
 
 import 'package:JobSearch/model/provider/storage/storage_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:http/http.dart' as http;
+
 
 final rawPictureProvider = AsyncNotifierProvider.autoDispose
     .family<StoragePictureNotifier, Uint8List?, String?>(
         StoragePictureNotifier.new);
 
 class StoragePictureNotifier
-    extends AutoDisposeFamilyAsyncNotifier<Uint8List?, String> {
+    extends AutoDisposeFamilyAsyncNotifier<Uint8List?, String?> {
   @override
-  FutureOr<Uint8List?> build(String? arg) async {
+  FutureOr<Uint8List?> build(String? arg) async { // arg is full path of the storage ref
     if (arg == null) {
       return null;
     }
@@ -30,8 +30,8 @@ class StoragePictureNotifier
       final fullRefPath = arg;
       final storage = ref.read(storageProvider);
       final storageRef = storage.ref(fullRefPath);
-      final downloadUrl = await storageRef.getDownloadURL();
-      final rawPicture = await http.readBytes(Uri.parse(downloadUrl));
+      // final downloadUrl = await storageRef.getDownloadURL();
+      final rawPicture = await storageRef.getData();
       return rawPicture;
     } catch (e) {
       return null;
