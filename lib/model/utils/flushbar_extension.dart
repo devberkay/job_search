26dc00ddd331c068fcb03d36367b6a11f839ea67
devberkay <canbr29@gmail.com>
2023-06-21 19:40:13 +0000

    Jun 21, 2023, 10:40 PM

--- lib/model/utils/flushbar_extension.dart
+++ lib/model/utils/flushbar_extension.dart
@@ -4,64 +4,74 @@ import 'package:flutter/material.dart';
 extension FlushBarExtension on BuildContext {
   showErrorFlushbar(String text) {
     Flushbar(
+      barBlur: 0.25,
+      positionOffset: 20,
       flushbarStyle: FlushbarStyle.FLOATING,
-      maxWidth: MediaQuery.of(this).size.width * 0.5,
+      maxWidth: MediaQuery.of(this).size.width * 0.3,
       duration: const Duration(seconds: 4),
       borderRadius:
           BorderRadius.circular(MediaQuery.of(this).size.height * 0.02),
-      margin: EdgeInsets.only(top: 15),
+      
       backgroundColor: Colors.red,
       messageText: Text(
         text,
+        overflow: TextOverflow.ellipsis,
         textAlign: TextAlign.center,
         style: TextStyle(
             fontWeight: FontWeight.w600,
             color: Colors.white,
             fontSize: MediaQuery.of(this).size.height * 0.0175),
       ),
-      flushbarPosition: FlushbarPosition.TOP,
+      flushbarPosition: FlushbarPosition.BOTTOM,
     ).show(this);
   }
 
   showSuccesFlashbar(String text) {
     Flushbar(
+      barBlur: 0.25,
+      positionOffset: 20,
       flushbarStyle: FlushbarStyle.FLOATING,
-      maxWidth: MediaQuery.of(this).size.width * 0.5,
+      maxWidth: MediaQuery.of(this).size.width * 0.3,
       duration: const Duration(seconds: 4),
-      margin: EdgeInsets.only(top: 15),
+      
       borderRadius:
           BorderRadius.circular(MediaQuery.of(this).size.height * 0.02),
       backgroundColor: Colors.green,
       messageText: Text(
         text,
         textAlign: TextAlign.center,
+        overflow: TextOverflow.ellipsis,
         style: TextStyle(
             fontWeight: FontWeight.bold,
             color: Colors.white,
             fontSize: MediaQuery.of(this).size.height * 0.0175),
       ),
-      flushbarPosition: FlushbarPosition.TOP,
+      flushbarPosition: FlushbarPosition.BOTTOM,
     ).show(this);
   }
 
   showInfoFlashbar(String text) {
     Flushbar(
+      barBlur: 0.25,
+      positionOffset: 20,
+      dismissDirection: FlushbarDismissDirection.HORIZONTAL,
       flushbarStyle: FlushbarStyle.FLOATING,
-      maxWidth: MediaQuery.of(this).size.width * 0.5,
+      maxWidth: MediaQuery.of(this).size.width * 0.3,
       duration: const Duration(seconds: 5),
-      margin: EdgeInsets.only(top: 15),
+      
       borderRadius:
           BorderRadius.circular(MediaQuery.of(this).size.height * 0.02),
       backgroundColor: Colors.black,
       messageText: Text(
         text,
+        overflow: TextOverflow.ellipsis,
         textAlign: TextAlign.center,
         style: TextStyle(
             fontWeight: FontWeight.bold,
             color: Colors.white,
             fontSize: MediaQuery.of(this).size.height * 0.0175),
       ),
-      flushbarPosition: FlushbarPosition.TOP,
+      flushbarPosition: FlushbarPosition.BOTTOM,
     ).show(this);
   }
 }
