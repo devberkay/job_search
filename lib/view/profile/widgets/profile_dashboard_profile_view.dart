d95db16f06c1232f8f89d8d90a93c2d08b6727cd
devberkay <canbr29@gmail.com>
2023-06-21 23:18:23 +0000

    Jun 22, 2023, 2:18 AM

--- lib/view/profile/widgets/profile_dashboard_profile_view.dart
+++ lib/view/profile/widgets/profile_dashboard_profile_view.dart
@@ -13,6 +13,7 @@ import 'package:JobSearch/view/shared/filled_cupertino_button.dart';
 import 'package:JobSearch/view/shared/headless_cupertino_button.dart';
 import 'package:JobSearch/view/shared/profile_avatar.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -81,15 +82,17 @@ class ProfileDashboardProfileView extends HookConsumerWidget {
                                         viewPort: const CroppieViewPort(
                                             type: 'circle'))
                                   ]);
-                                
+
                               final croppedImage =
                                   await croppedFile!.readAsBytes();
-                            
+
                               ref
                                   .read(uploadServiceProvider.notifier)
                                   .uploadPicture(
                                       rawPicture: croppedImage,
-                                      userId: userModel.uid);
+                                      userId: userModel.uid,
+                                      settableMetadata: SettableMetadata(
+                                          contentType: xFile.mimeType));
                             }
                           } catch (e) {
                             context.showErrorFlushbar(
@@ -167,11 +170,13 @@ class ProfileDashboardProfileView extends HookConsumerWidget {
                             );
                             if (pickedFile != null &&
                                 pickedFile.files.isNotEmpty) {
-                             await ref
+                              await ref
                                   .read(uploadServiceProvider.notifier)
                                   .uploadFile(
                                       cvFile: pickedFile.files.first.bytes!,
-                                      userId: userModel.uid);
+                                      userId: userModel.uid,settableMetadata: SettableMetadata(
+                                          contentType: pickedFile.files.first.extension
+                                      ));
                             }
                           } catch (e) {
                             debugPrint("heyerror :$e");
