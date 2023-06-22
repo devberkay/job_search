ff7ae3accc28c4d02f0a1866b1c3fb6b78c65773
devberkay <canbr29@gmail.com>
2023-06-22 18:00:37 +0000

    Jun 22, 2023, 9:00 PM

--- lib/model/service/firestore/manage_job_post_service.dart
+++ lib/model/service/firestore/manage_job_post_service.dart
@@ -2,3 +2,9 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 
 
+class ManageJobPostServiceStreamNotifier extends StreamNotifier<> {
+  @override
+  Stream<> build() {
+    return ;
+  }
+}
