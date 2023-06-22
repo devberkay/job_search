23534178c8b7911e479d1dd9bb7136903b0f1e1f
devberkay <canbr29@gmail.com>
2023-06-22 19:06:07 +0000

    Jun 22, 2023, 10:06 PM

--- lib/model/service/firestore/manage_job_post_service.dart
+++ lib/model/service/firestore/manage_job_post_service.dart
@@ -1,22 +1,53 @@
+import 'package:JobSearch/model/data/application_model.dart';
+import 'package:JobSearch/model/data/job_model.dart';
 import 'package:JobSearch/model/data/merged_manage_job_post_model.dart';
+import 'package:JobSearch/model/data/user_model.dart';
 import 'package:JobSearch/model/provider/auth/user_provider.dart';
 import 'package:JobSearch/model/provider/firestore/firestore_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ManageJobPostServiceStreamNotifier
-    extends StreamNotifier<MergedManageJobPostModel?> {
+    extends StreamNotifier<List<MergedManageJobPostModel>?> {
   @override
-  Stream<MergedManageJobPostModel?> build() async* {
+  Stream<List<MergedManageJobPostModel>?> build() async* {
+    var applicationModels = <ApplicationModel>[];
+    var jobModels = <JobModel>[];
+    var applicantUserModels = <UserModel>[];
+    var mergedModels = <MergedManageJobPostModel>[];
     final firestore = ref.read(firestoreProvider);
     final selfUserId = ref.read(userProvider)!.uid;
-    final jobPostStream = firestore
-        .collection('jobPosts')
-        .where('owner', isEqualTo: selfUserId)
-        .where('isActive', isEqualTo: true)
-        .snapshots();
-    
-    await for (final jobs in jobPostStream) {
-      await firestore.collection("applications").where("jobId",arrayContains: jobs.docs.map((e) => e.id)).get();
+    final applicationsStream = firestore
+        .collection('applications')
+        .where('ownerUid', isEqualTo: selfUserId)
+        .where('status', isEqualTo: null)
+        .snapshots(); // we get the ApplicationModels here
+
+    await for (final assessableJobs in applicationsStream) {
+      applicationModels = assessableJobs.docs
+          .map((e) => ApplicationModel.fromJson(e.data()))
+          .toList();
+      // secondly get the usermodel of applicant
+      final applicantUserModelQuery = await firestore
+          .collection('users')
+          .where('uid', whereIn: applicationModels.map((e) => e.uid).toList())
+          .get();
+      applicantUserModels = applicantUserModelQuery.docs
+          .map((e) => UserModel.fromJson(e.data()))
+          .toList();
+      // third get the related jobmodel
+      final jobModelQuery = await firestore
+          .collection('jobPosts')
+          .where('jobId',
+              whereIn: applicationModels.map((e) => e.jobId).toList())
+          .get();
+      jobModels =
+          jobModelQuery.docs.map((e) => JobModel.fromJson(e.data())).toList();
+
+      // now create the list of  merged model
+      for (final applicationModel in applicationModels) {
+        
+      }
+      yield mergedModels;
     }
   }
 }
