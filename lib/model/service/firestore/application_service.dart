0bb4f0418cb5c539e77fadac16035eefeb867fa5
devberkay <canbr29@gmail.com>
2023-06-22 21:03:38 +0000

    Jun 23, 2023, 12:03 AM

--- lib/model/service/firestore/application_service.dart
+++ lib/model/service/firestore/application_service.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:JobSearch/model/data/job_model.dart';
 import 'package:JobSearch/model/provider/firestore/firestore_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -7,10 +8,14 @@ class ApplicatioNServiceNotifier extends AsyncNotifier<void> {
   @override
   FutureOr<void> build() {}
 
-  Future<void> applyJob() async {
+  Future<void> applyJob({required String jobId}) async {
     final firestore = ref.read(firestoreProvider);
     final applicationCollectionRef = firestore.collection('applications');
-    final jobPostsCollectionRef = firestore.collection('jobPosts');
-    
+    final jobPostsCollectionDocRef =
+        firestore.collection('jobPosts').doc(jobId);
+    final jobPostQuery = await jobPostsCollectionDocRef.get();
+    final jobPost = JobModel.fromJson(jobPostQuery.data()!);
+    final newJobPost =
+        jobPost.copyWith(applicantCounter: jobPost.applicantCounter + 1);
   }
 }
