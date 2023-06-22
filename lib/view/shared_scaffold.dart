290fd1af49f08434230d8372967e2a3c74ba176c
devberkay <canbr29@gmail.com>
2023-06-22 06:38:08 +0000

    Jun 22, 2023, 9:38 AM

--- lib/view/shared_scaffold.dart
+++ lib/view/shared_scaffold.dart
@@ -2,10 +2,16 @@ import 'package:JobSearch/model/service/firestore/user_model_service_notifier.da
 import 'package:JobSearch/model/service/storage/upload_service.dart';
 import 'package:JobSearch/model/utils/flushbar_extension.dart';
 import 'package:JobSearch/view/home/widgets/home_page_appbar.dart';
+import 'package:JobSearch/view/profile/widgets/profile_sidebar.dart';
+import 'package:JobSearch/view/shared/headless_cupertino_button.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:loader_overlay/loader_overlay.dart';
+import 'package:mime/mime.dart';
 
 class SharedScaffold extends StatefulHookConsumerWidget {
   const SharedScaffold(
@@ -42,6 +48,89 @@ class _SharedScaffoldState extends ConsumerState<SharedScaffold> {
     });
     return SafeArea(
       child: Scaffold(
+        floatingActionButton: widget.routerState.location.contains('profile') &&
+                ref.watch(profileSidebarIndexProvider) == 0
+            ? FloatingActionButton(
+                onPressed: () async {
+                  try {
+                    final pickedFile = await FilePicker.platform.pickFiles(
+                      type: FileType.custom,
+                      allowedExtensions: ['pdf'],
+                    );
+                    if (pickedFile != null && pickedFile.files.isNotEmpty) {
+                      await ref.read(uploadServiceProvider.notifier).uploadFile(
+                          cvFile: pickedFile.files.first.bytes!,
+                          userId: widget.routerState.pathParameters['userId']!,
+                          settableMetadata: SettableMetadata(
+                              contentType: lookupMimeType(
+                                  pickedFile.files.first.extension!),
+                              customMetadata: {
+                                "name": pickedFile.files.first.name
+                              }));
+                    }
+                  } catch (e) {
+                    debugPrint("heyerror :$e");
+                    context.showErrorFlushbar("File could not be selected");
+                  }
+                },
+                child: HookConsumer(builder: (context, ref, child) {
+                  final opacityNotifier = useValueNotifier(0.0);
+                  return MouseRegion(
+                      onExit: (event) {
+                        opacityNotifier.value = 0.0;
+                      },
+                      onEnter: (event) {
+                        opacityNotifier.value = 1.0;
+                      },
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Container(
+                            width: 150,
+                            height: 150,
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Colors.black),
+                                color: Colors.white,
+                                shape: BoxShape.circle),
+                            child: const Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text("CV",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 75 / 4,
+                                        fontWeight: FontWeight.w900)),
+                                Icon(Icons.download,
+                                    color: Colors.black, size: 75 / 4)
+                              ],
+                            ),
+                          ),
+                          ValueListenableBuilder(
+                              valueListenable: opacityNotifier,
+                              builder: (context, opacity, child) {
+                                return AnimatedOpacity(
+                                    opacity: opacity,
+                                    curve: Curves.easeOut,
+                                    duration: const Duration(milliseconds: 250),
+                                    child: Container(
+                                        width: 150,
+                                        height: 150,
+                                        alignment: Alignment.center,
+                                        decoration: BoxDecoration(
+                                            color:
+                                                Colors.black.withOpacity(0.5),
+                                            shape: BoxShape.circle),
+                                        child: const Icon(
+                                          Icons.file_upload_outlined,
+                                          color: Colors.white,
+                                          size: 75 / 2,
+                                        )));
+                              }),
+                        ],
+                      ));
+                }),
+              )
+            : null,
         appBar: const HomePageAppBar(
           customPreferredSize: Size(double.infinity, 60),
         ),
