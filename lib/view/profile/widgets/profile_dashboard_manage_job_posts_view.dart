d434170eb53228cf843d2b1df445efc8563c5541
devberkay <canbr29@gmail.com>
2023-06-22 19:23:37 +0000

    Jun 22, 2023, 10:23 PM

--- lib/view/profile/widgets/profile_dashboard_manage_job_posts_view.dart
+++ lib/view/profile/widgets/profile_dashboard_manage_job_posts_view.dart
@@ -1,10 +1,38 @@
+import 'package:JobSearch/model/service/firestore/manage_job_post_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ProfileDashboardManageJobPostsView extends HookConsumerWidget {
   const ProfileDashboardManageJobPostsView({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    final manageJobPostMergedModels =
+        ref.watch(manageJobPostMergedModelProvider);
+    return manageJobPostMergedModels.when(data: (_manageJobPostMergedModels) {
+      return ListView.separated(
+          itemCount: _manageJobPostMergedModels!.length,
+          separatorBuilder: (context, index) {
+            return SizedBox(height: 30);
+          },
+          itemBuilder: ((context, index) {
+            _manageJobPostMergedModels.map((e) => null);
+          }));
+    }, error: (e, st) {
+      return const Center(
+          child: Column(
+        children: [
+          Icon(Icons.error),
+          SizedBox(height: 10),
+          Text("We are facing an issue right now. Please try again later"),
+        ],
+      ));
+    }, loading: () {
+      return const Center(
+        child: SpinKitRing(
+          color: Colors.black,
+        ),
+      );
+    });
   }
 }
