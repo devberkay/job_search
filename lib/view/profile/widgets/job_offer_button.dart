0bea1d044f40596f5031076c0fdfa6ec891641d3
devberkay <canbr29@gmail.com>
2023-06-23 08:57:08 +0000

    Jun 23, 2023, 11:57 AM

--- lib/view/profile/widgets/job_offer_button.dart
+++ lib/view/profile/widgets/job_offer_button.dart
@@ -7,6 +7,64 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class JobOfferButton extends HookConsumerWidget {
   const JobOfferButton({super.key});
+
+  void showCustomDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      barrierColor: Colors.black54,
+      builder: (BuildContext context) {
+        return Dialog(
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: Container(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      title: Text('Item $index'),
+                      // Add your custom logic here for item selection
+                    );
+                  },
+                ),
+                SizedBox(height: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Add your custom logic for the "Select" button
+                      },
+                      child: Text(
+                        'Select',
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(
+                        'Cancel',
+                        style: TextStyle(color: Colors.red),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return HookConsumer(builder: (context, ref, child) {
@@ -21,7 +79,7 @@ class JobOfferButton extends HookConsumerWidget {
           },
           child: HeadlessCupertinoButton(
             onPressed: () async {
-              
+              showCustomDialog(context);
             },
             child: Stack(
               alignment: Alignment.center,
