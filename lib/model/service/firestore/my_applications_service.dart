0dd56fc7cd47c4b640b6be1eeb31bcdd9c15555e
devberkay <canbr29@gmail.com>
2023-06-23 00:34:10 +0000

    Jun 23, 2023, 3:34 AM

--- lib/model/service/firestore/my_applications_service.dart
+++ lib/model/service/firestore/my_applications_service.dart
@@ -1,13 +1,15 @@
-
-
-
-
 import 'package:JobSearch/model/data/application_model.dart';
+import 'package:JobSearch/model/provider/auth/user_provider.dart';
+import 'package:JobSearch/model/provider/firestore/firestore_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class MyApplicationsNotifier extends StreamNotifier<List<ApplicationModel>> {
   @override
-  Stream<List<ApplicationModel>> build() {
-    return ;
+  Stream<List<ApplicationModel>> build() async* {
+    final firestore = ref.read(firestoreProvider);
+    final selfUser = ref.read(userProvider);
+    final applicationsCollectionRef = firestore.collection('applications');
+    final applicationsStream = applicationsCollectionRef.where(selfUser)
+    await for ()
   }
 }
