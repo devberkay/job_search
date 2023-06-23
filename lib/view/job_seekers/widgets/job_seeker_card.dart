6b8d1fda24dd01aceff6fb35b13def1076b01728
devberkay <canbr29@gmail.com>
2023-06-23 06:58:56 +0000

    Jun 23, 2023, 9:58 AM

--- lib/view/job_seekers/widgets/job_seeker_card.dart
+++ lib/view/job_seekers/widgets/job_seeker_card.dart
@@ -7,6 +7,7 @@ import 'package:JobSearch/view/shared/profile_avatar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class JobSeekerCard extends HookConsumerWidget {
@@ -14,13 +15,122 @@ class JobSeekerCard extends HookConsumerWidget {
   final UserModel userModel;
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ListTile(
-      leading: Text(
-        userModel.positionTitles.join(),
-        style: TextStyle(fontWeight: FontWeight.w900),
+    final isHovering = useState(false);
+    return MouseRegion(
+      onEnter: (event) {
+        isHovering.value = true;
+      },
+      onExit: (event) {
+        isHovering.value = false;
+      },
+      child: InkWell(
+        onTap: () {
+          context.go('/profile/${userModel.uid}');
+        },
+        child: AnimatedContainer(
+          clipBehavior: Clip.none,
+          duration: const Duration(milliseconds: 250),
+          transformAlignment: Alignment.center,
+                      curve: Curves.easeOut,
+            height: 250,
+            width: double.maxFinite,
+            padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 25),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey.shade300),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.shade500,
+                      offset: Offset(
+                          isHovering.value ? -1 : 0, isHovering.value ? -1 : 0),
+                      blurRadius: isHovering.value ? 1.25 : 0,
+                      spreadRadius: isHovering.value ? 1.25 : 0),
+                  BoxShadow(
+                      color: Colors.grey.shade500,
+                      offset: Offset(
+                          isHovering.value ? 1 : 0, isHovering.value ? 1 : 0),
+                      blurRadius: isHovering.value ? 1.25 : 0,
+                      spreadRadius: isHovering.value ? 1.25 : 0)
+                ]),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(jobModel.title,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.w800, fontSize: 25)),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text("$companyEmoji ${jobModel.organization}",
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 14,
+                                    color: Colors.black)),
+                            const SizedBox(width: 10),
+                            Text(
+                                "${jobModel.isRemote ? computerEmoji : locationEmoji} ${jobModel.isRemote ? "Remote available" : 'In-office'}",
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 14,
+                                    color: Colors.black)),
+                          ],
+                        )
+                      ],
+                    ),
+                    const Spacer(),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(top: 5),
+                          child: ProfileBox(
+                              height: 40, width: 100, userId: jobModel.owner),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 2),
+                          child: Row(
+                            children: [
+                              Icon(Icons.update, size: 15, color: Colors.black),
+                              const SizedBox(width: 3),
+                              Text(
+                                  _convertToCustomFormat(
+                                      (jobModel.timestampField as Timestamp)
+                                          .toDate()),
+                                  style: TextStyle(
+                                      fontSize: 9 * 1.25,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w600))
+                            ],
+                          ),
+                        ),
+                        const SizedBox(width: 30),
+                        Text("$moneyEmoji \$${jobModel.salaryPerHour}/hr",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 14,
+                                color: usDollarGreen)),
+                        const SizedBox(width: 10),
+                        Text(
+                            "$applicantsEmoji ${jobModel.applicantCounter} applied",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 14,
+                                color: Colors.blueAccent.shade700))
+                      ],
+                    )
+                  ],
+                ),
+                  ],
+                ),
+        ),
       ),
-      title: Text("${userModel.name} ${userModel.surname}",
-          style: TextStyle(fontWeight: FontWeight.w600)),
     );
   }
 }
