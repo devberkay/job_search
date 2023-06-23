12d10c6dd50446935cc6618e5a61160dbe5bff24
devberkay <canbr29@gmail.com>
2023-06-23 01:50:24 +0000

    Jun 23, 2023, 4:50 AM

---
+++ lib/model/service/firestore/offer_listener_stream_notifier.dart
@@ -0,0 +1,40 @@
+import 'package:JobSearch/model/data/job_offer_model.dart';
+import 'package:JobSearch/model/provider/auth/user_provider.dart';
+import 'package:JobSearch/model/provider/firestore/firestore_provider.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final offerListenerStreamNotifierProvider = StreamNotifierProvider.autoDispose.family<OfferListenerStreamNotifier,List<JobOfferModel>,String>(OfferListenerStreamNotifier.new);
+
+class OfferListenerStreamNotifier
+    extends AutoDisposeFamilyStreamNotifier<List<JobOfferModel>, String> {
+  // arg is for listening type : "INCOMING" or "OUTGOING"
+  @override
+  Stream<List<JobOfferModel>> build(String arg) async* {
+    final firestore = ref.read(firestoreProvider);
+    final selfUserId = ref.read(userProvider)!.uid;
+    final offerCollectionRef = firestore.collection('offers');
+    if (arg == "OUTGOING") {
+      final offerStream = offerCollectionRef
+          .where('senderUid', isEqualTo: selfUserId)
+          .snapshots();
+      await for (final offerQuery in offerStream) {
+        final offerList = offerQuery.docs
+            .map((e) => JobOfferModel.fromJson(e.data()))
+            .toList();
+        yield offerList;
+      }
+    } else if (arg == "INCOMING") {
+      final offerStream = offerCollectionRef
+          .where('recipientUid', isEqualTo: selfUserId)
+          .snapshots();
+      await for (final offerQuery in offerStream) {
+        final offerList = offerQuery.docs
+            .map((e) => JobOfferModel.fromJson(e.data()))
+            .toList();
+        yield offerList;
+      }
+    } else {
+      throw Exception("Invalid argument for OfferListenerStreamNotifier");
+    }
+  }
+}
--- lib/model/service/firestore/offer_service.dart
+++ lib/model/service/firestore/offer_service.dart
@@ -1,15 +1,31 @@
 import 'dart:async';
 
+import 'package:JobSearch/model/data/job_offer_model.dart';
+import 'package:JobSearch/model/provider/auth/user_provider.dart';
 import 'package:JobSearch/model/provider/firestore/firestore_provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class OfferServiceNotifier extends AsyncNotifier<void> {
+class OfferServiceNotifier extends AsyncNotifier<String?> {
   @override
-  FutureOr<void> build() {}
+  FutureOr<String?> build() {
+    return null;
+  }
 
-  Future<void> offerJob() async {
+  Future<void> sendJobOffer(
+      {required String recipientUserId, required String jobPostId}) {
     final firestore = ref.read(firestoreProvider);
+    final selfUserId = ref.read(userProvider)!.uid;
     final offerCollectionRef = firestore.collection('offers');
-    
+    final offerDocumentRef = offerCollectionRef.doc();
+    final offerId = offerDocumentRef.id;
+    final offerModel = JobOfferModel(
+      jobOfferId: offerId,
+        senderUid: selfUserId,
+        recipientUid: recipientUserId,
+        jobId: jobPostId,
+        timestampField: FieldValue.serverTimestamp(),
+        status: null);
+    return offerDocumentRef.set(offerModel.toJson());
   }
 }
