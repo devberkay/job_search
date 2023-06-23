20df142d5bd00a258f60eed064f1b02a1199b4b3
devberkay <canbr29@gmail.com>
2023-06-23 00:31:10 +0000

    Jun 23, 2023, 3:31 AM

---
+++ lib/model/service/firestore/my_applications_service.dart
@@ -0,0 +1,13 @@
+
+
+
+
+import 'package:JobSearch/model/data/application_model.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class MyApplicationsNotifier extends StreamNotifier<List<ApplicationModel>> {
+  @override
+  Stream<List<ApplicationModel>> build() {
+    return ;
+  }
+}
